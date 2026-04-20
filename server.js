caadba16fd7485e88cbdc445817b17de2d7ca336
ouraX-PRO <manishstifler2424@gmail.com>
2026-04-20 12:46:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ const cors       = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
 app.use(express.json());
 
 // ─────────────────────────────────────
